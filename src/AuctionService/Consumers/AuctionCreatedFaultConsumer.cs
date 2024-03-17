f8c32c7d797f70bff8c39de455f111198610356e
Igor Fialkov <github@shtilkind.net>
2024-03-17 09:03:29 -0700

    Added Identity Server

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine($"AuctionCreatedFaultConsumer: {context.Message.Message.Id}");
+
+        var exception = context.Message.Exceptions.First();
+
+        if(exception.ExceptionType == typeof(ArgumentException).FullName)
+        {
+            context.Message.Message.Model = "FooBar";
+            await context.Publish(context.Message.Message);
+        }
+        else
+        {
+            Console.WriteLine($"AuctionCreatedFaultConsumer: {exception.Message}");
+        }
+    }
+}
