f7212c8b234e8ca76baf3720a478bf0ec03e86f6
Igor Fialkov <github@shtilkind.net>
2024-03-22 09:08:38 -0700

    Working on lesson 118. Before installing form packages.

--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -4,19 +4,24 @@ import { decl } from "postcss";
 declare module 'next-auth' {
     interface Session extends DefaultSession {
         user: {
-            username: string;
-        } & DefaultSession['user'];
+            id: string
+            username: string
+        } & DefaultSession['user']
     }
 
     interface Profile {
-        username: string;
+        username: string
+    }
+
+    interface User {
+        username: string
     }
 }
 
 declare module 'next-auth/jwt' {
     interface JWT {
-        username: string;
-        access_token?: string;
+        username: string
+        access_token?: string
     }
 }
 
