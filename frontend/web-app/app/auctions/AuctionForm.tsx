db4f4f26ce16865365396bdbdde5ed829c615c1b
Igor Fialkov <github@shtilkind.net>
2024-03-22 23:24:21 -0700

    End of lesson 124. Implemented Create an Auction

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,5 +1,115 @@
-import React from "react";
+"use client";
+
+import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
+import { Button } from "flowbite-react";
+import { useEffect } from "react";
+import DateInput from "../components/DateInput";
+import { createAuction } from "../actions/auctionActions";
+import { useRouter } from "next/navigation";
 
 export default function AuctionForm() {
-  return <div>Auction Form goes here</div>;
+  const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid },
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  useEffect(() => {
+    setFocus("make");
+  }, [setFocus]);
+
+  async function onSubmit(data: FieldValues) {
+    try {
+      const res = await createAuction(data);
+      if (res.error) {
+        throw new Error(res.error);
+      }
+      router.push(`/auctions/details/${res.id}`);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  return (
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label="Color"
+        name="color"
+        control={control}
+        rules={{ required: "Color is required" }}
+      />
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Year"
+          name="year"
+          control={control}
+          type="number"
+          rules={{ required: "Year is required" }}
+        />
+        <Input
+          label="Mileage"
+          name="mileage"
+          type="number"
+          control={control}
+          rules={{ required: "Mileage is required" }}
+        />
+      </div>
+
+      <Input
+        label="Image URL"
+        name="imageUrl"
+        control={control}
+        rules={{ required: "Image URL is required" }}
+      />
+
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Reserve Price (enter 0 if no reserve)"
+          name="reservePrice"
+          control={control}
+          type="number"
+          rules={{ required: "Reserve Price is required" }}
+        />
+        <DateInput
+          label="Auction End date/time"
+          name="auctionEnd"
+          control={control}
+          dateFormat="dd MMMM yyyy h:mm a"
+          showTimeSelect
+          rules={{ required: "Auction End date is required" }}
+        />
+      </div>
+      <div className="flex justify-between">
+        <Button outline color="gray">
+          Cancel
+        </Button>
+        <Button
+          isProcessing={isSubmitting}
+          disabled={!isValid}
+          type="submit"
+          outline
+          color="success"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
 }
---
+++ frontend/web-app/app/components/DateInput.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { UseControllerProps, useController } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
+import Datepicker, { ReactDatePickerProps } from "react-datepicker";
+
+type Props = {
+  label: string;
+  type?: string;
+  showLabel?: boolean;
+} & UseControllerProps &
+  Partial<ReactDatePickerProps>;
+
+export default function DateInput(props: Props) {
+  const { fieldState, field } = useController({ ...props, defaultValue: "" });
+  return (
+    <div className="block">
+      <Datepicker
+        {...props}
+        {...field}
+        onChange={(value) => field.onChange(value)}
+        selected={field.value}
+        placeholderText={props.label}
+        className={`
+        rounded-lg w-[100%] flex flex-col
+        ${
+          fieldState.error
+            ? "border-red-500 bg-red-50 text-red-900"
+            : !fieldState.invalid && fieldState.isDirty
+            ? "border-green-500 bg-green-50 text-green-900"
+            : ""
+        }
+        `}
+      />
+      {fieldState.error && (
+        <div className="text-sm text-red-500 mt-1">
+          {fieldState.error.message}
+        </div>
+      )}
+    </div>
+  );
+}
