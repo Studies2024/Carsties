001aee9eecd413cae5fd73423b94e2c3e388ab5a
Igor Fialkov <github@shtilkind.net>
2024-03-25 11:09:35 -0700

    End of Section 11

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
+using AuctionService.Services;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -46,6 +47,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 
 
@@ -55,6 +58,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
---
+++ src/BiddingService/Services/GrpcAuctionClient.cs
@@ -0,0 +1,44 @@
+﻿using BiddingService.Models;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace BiddingService.Services;
+
+public class GrpcAuctionClient
+{
+    private readonly ILogger<GrpcAuctionClient> _logger;
+    private readonly IConfiguration _config;
+
+    public GrpcAuctionClient(ILogger<GrpcAuctionClient> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public Auction GetAuction(string id)
+    {
+        _logger.LogInformation("Getting auction with id {id}", id);
+
+        var channel = GrpcChannel.ForAddress(_config["GrpcAuction"]);
+        var client = new GrpcAuction.GrpcAuctionClient ( channel );
+        var request = new GetAuctionRequest { Id = id };
+
+        try
+        {
+            var response = client.GetAuction(request);
+            var auction = new Auction
+            {
+                ID = response.Auction.Id,
+                AuctionEnd = DateTime.Parse(response.Auction.AuctionEnd),
+                Seller = response.Auction.Seller,
+                ReservePrice = Int32.Parse(response.Auction.ReservePrice)
+            };
+            return auction;
+        }
+        catch (RpcException ex)
+        {
+            _logger.LogError("Error getting auction with id {id}: {e}", id, ex);
+            return null;
+        }
+    }
+}
